cce00755e04564825bd8eb1f9bcc3154add71254
stalactitecorp <117663645+stalactitecorp@users.noreply.github.com>
2022-12-17 00:12:13 +0200

    Add files via upload

--- contact.py
+++ contact.py
@@ -23,92 +23,55 @@ def parse_file(file_name):
     line = str(file_name)
     while line != "":
         line = data_set.readline().rstrip()
-        infect_dict.update({line.split(',')[0]: line.split(',')[1:]})
+        infect_dict.update({line.split(',')[0]: sorted(line.split(',')[1:])})
     data_set.close()
     infect_dict.pop('', None)
     #The above line is simply a precaution as sometimes one extra line on the file which is empty might be added. The above removes it from dictionary.
-    print(infect_dict)
     return infect_dict
 
 
+def dic_values(contacts_dic):
 
-
-    """Read the input file, parse the contents and return a dictionary
-    containing sick people and their contacts.
-
-    Args:
-        file_name (str): Contains the name of the file.
-
-    Returns:
-        dict: Contains contact tracing information. The keys are the sick
-        people. The corresponding values are stored in a list that contains
-        the names of all the people that the sick person has had contact with.
-    """
-
-
-
-
-def find_patients_zero(contacts_dic):
-
-# MAIN IDEA: The people who do not appear in any sick person's contacts have to be in the keys of the dictionary, hence it is sufficient if each key is not in all the values.
-
-
-# My initial idea was to only have the second for loop and instead of writing "not in values" to write "not in list(contacts_dic.values())"
-# Essentially this would check over all values and the code would be finished however since the values are lists, I would have a nested list and would therefore compare a string with a list
-# Hence the first for loop transforms this nested list of values into a one-dimensional list with values
-
-    patients_zero = []
-    dic_values = []
-    for sublist in list(contacts_dic.values()):
-        for element in sublist:
-            dic_values.append(element) 
-
-    for key in range(len(contacts_dic)):
-        if list(contacts_dic.keys())[key] not in dic_values:
-            patients_zero.append(list(contacts_dic.keys())[key])
-    return sorted(patients_zero)
-    
-
-    """Return list of people who do not appear in any sick person's contact
-    list.
-
-    Args:
-        contacts_dic (dic): each entry is a sick person's name (key) and their
-        list of contacts.
-
-    Returns:
-        list: names of people who do not appear in any sick person's contact
-        list.
-    """
-    return [] # delete this line
-
-
-def find_potential_zombies(contacts_dic):
-
-# Again, start by creating a list containing all dictionary values
-    potential_zombies = []
-    dic_values = []
-    for sublist in list(contacts_dic.values()):
-        for element in sublist:
-            dic_values.append(element) 
-
-# Now for each element in this list check whether it matches with any of the keys, if not then it is a potential zombie
-
-    for element in dic_values:
-        if element not in list(contacts_dic.keys()):
-            potential_zombies.append(element)
-    print(sorted(list(set(potential_zombies))))
-    return sorted(list(set(potential_zombies)))
-    """Return list of people who do not appear to be sick yet. They appear in
-    the contact lists but do not have their own contacts entry.
+    """Return all people who have been contacted by a zombie.
 
     Args:
         contacts_dic (dic): each entry is a sick person's name and their list
         of contacts.
 
     Returns:
-        list: names of people who are not listed as sick.
+        list: all names (repetitions included) in all values associated with every key of the input dictionary.
     """
+    
+    dic_values = []
+    for sublist in list(contacts_dic.values()):
+        for element in sublist:
+            dic_values.append(element)
+    return dic_values
+
+def find_patients_zero(contacts_dic):
+
+# MAIN IDEA: The people who do not appear in any sick person's contacts have to be in the keys of the dictionary, hence it is sufficient if each key is not in all the values.
+    patients_zero = []
+    dict_values = dic_values(contacts_dic)
+
+    for key in range(len(contacts_dic)):
+        if list(contacts_dic.keys())[key] not in dict_values:
+            patients_zero.append(list(contacts_dic.keys())[key])
+    return sorted(patients_zero)
+    
+
+
+def find_potential_zombies(contacts_dic):
+
+    potential_zombies = []
+    dict_values = dic_values(contacts_dic)
+
+# Now for each element in this list check whether it matches with any of the keys, if not then it is a potential zombie
+
+    for element in dict_values:
+        if element not in list(contacts_dic.keys()):
+            potential_zombies.append(element)
+    return sorted(list(set(potential_zombies)))
 
 
 def find_not_zombie_nor_zero(contacts_dic, patients_zero_list, zombie_list):
@@ -117,34 +80,11 @@ def find_not_zombie_nor_zero(contacts_dic, patients_zero_list, zombie_list):
 #combined_zombie is the list containing patient zeroe(s) and potential zombies
 #Finally, the list returned is all the names (combined_dic) minus the names in combined_zombie
     
-    dic_values = []
-    for sublist in list(contacts_dic.values()):
-        for element in sublist:
-            dic_values.append(element) 
-
+    dict_values = dic_values(contacts_dic)
     combined_zombie = list(set(zombie_list + patients_zero_list))
-    combined_dic = list(set(list(contacts_dic.keys()) + dic_values))
-
-    
+    combined_dic = list(set(list(contacts_dic.keys()) + dict_values))
     return sorted([x for x in combined_dic if x not in combined_zombie])
 
-        
-
-
-    
-    """Return names of those people who are neither a zombie nor a patient
-    zero.
-
-    Args:
-        contacts_dic (dic): each entry is a sick person's name and their list
-        of contacts.
-        patients_zero_list (list): sick people identified as patient zero(es).
-        zombie_list (list): contacts who are not a sick person (don't have
-        their own contact list).
-
-    Returns:
-        list: people who are neither a zombie nor a patient zero.
-    """
     
 
 
@@ -152,40 +92,26 @@ def find_most_viral(contacts_dic):
 
     maximum_contacts = max([len(x) for x in list(contacts_dic.values())])
     return sorted([x for x in list(contacts_dic.keys()) if len(contacts_dic[str(x)]) == maximum_contacts])
-    
-
-    """Return the most viral contacts: those sick people with the largest
-    contact list
-
-    Args:
-        contacts_dic (dic): each entry is a sick person's name and their list
-        of contacts.
-
-    Returns:
-        list: contains the names of sick people who have the largest contact
-        lists
-    """
 
 
 
 def find_most_contacted(contacts_dic):
 
-#dic_values once again returns a list with all contacts for every key
 
-    dic_values = []
-    for sublist in list(contacts_dic.values()):
-        for element in sublist:
-            dic_values.append(element)
+    dict_values = dic_values(contacts_dic)
+
 
 #Now remove all duplicates by making dic_values a set and then count each element of that set in the original dic_values list
 #Retrieve the maximum count
 #Finally return the names in dic_values that appear 'maximum_appearance' times
 
-    max_appearance = max([dic_values.count(list(set(dic_values))[x]) for x in range(len(set(dic_values)))])
-    return sorted(list(set([x for x in dic_values if dic_values.count(x) == max_appearance])))
+    max_appearance = max([dict_values.count(list(set(dict_values))[x]) for x in range(len(set(dict_values)))])
+    return sorted(list(set([x for x in dict_values if dict_values.count(x) == max_appearance])))
     
 
 
+
+def auxiliary_recursion_func(contacts_dic, zombie_list, dict_names):
     
     """Return the contact or contacts who appear in the most sick persons'
     contact list.
@@ -193,14 +119,14 @@ def find_most_contacted(contacts_dic):
     Args:
         contacts_dic (dic): each entry is a sick person's name and their list
         of contacts.
+        zombie_list (list): list of potential zombies
+        dict_names (dic): The keys are all names in a data set and the values empty lists
 
     Returns:
-        list: contains the names of contacts who appear in the most sick
-        persons' contact list.
+        dictionary: contains the names of everyone in the dataset as keys and
+        a list as value containing the number of people contacted including their height.
     """
 
-def auxiliary_recursion_func(contacts_dic, zombie_list, dict_names):
-    
     #Recursive solution to SECTION 10
     if len(contacts_dic) == 0:
         for i in dict_names:
@@ -232,28 +158,10 @@ def find_maximum_distance_from_zombie(contacts_dic, zombie_list):
     return auxiliary_recursion_func(contacts_dic, zombie_list, dict_names)
 
 
-    """Return the maximum distance from a zombie for everyone in the dataset.
-    The maximum distance from a potential zombie is the longest contact
-    tracing path downwards in the dataset from a sick person to a potential
-    zombie.
 
-    Args:
-        contacts_dic (dic): each entry is a sick person's name and their
-        list of contacts.
-        zombie_list (list): all zombies
 
-    Returns:
-        dic: contains heights (maximum distance) of person from a zombie
-    """
 
-def dic_values(contacts_dic):
-    
-    #Function that creates a list of values associated with every key in a given dictionary
-    dic_values = []
-    for sublist in list(contacts_dic.values()):
-        for element in sublist:
-            dic_values.append(element)
-    return dic_values
+
 
 
     
@@ -286,6 +194,7 @@ def main():
     # Section 3. Create contacts dictionary from the file
     # I iterate over each key in the contacts dictionary and then concatenate that with "had contact with" and finally I call the format_list and apply it on the value of each key.
     contacts_dic = parse_file(filename)
+    print(contacts_dic)
     for key in range(len(contacts_dic)):
         print(list(contacts_dic.keys())[key] + " had contact with " +  format_list(contacts_dic[list(contacts_dic.keys())[key]]))
     
@@ -345,6 +254,7 @@ def main():
     ######
     # Extra functionality - no function headers provided.
     ######
+    
 
 if __name__ == "__main__":
     main()
